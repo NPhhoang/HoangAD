5de2e23be6d8e4ee287d294588890d797d580e1c
NPhHoang <hoangdqnp@gmail.com>
2019-12-03 16:33:53 +0700

    master

--- scrCom/js/main.js
+++ scrCom/js/main.js
@@ -70,7 +70,7 @@ loader.load('scrCom/sketch/vvt.dae', function(apart){
     // var material = new THREE.MeshFaceMaterial(null);
     // apartModel = new THREE.Mesh(geometry, material);
     // apart.scale.set(0.5,0.5,0.5);
-    apart.position.y +=0.5;
+    // apart.position.y +=0.5;
     scene.add(apart);
 });
 renderer.render(scene,camera);
