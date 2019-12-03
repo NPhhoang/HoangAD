0b41837d04670d8606c94daecf84b3698f043e31
NPhHoang <hoangdqnp@gmail.com>
2019-12-03 23:30:58 +0700

    master

--- scrCom/js/main.js
+++ scrCom/js/main.js
@@ -4,6 +4,10 @@ function init() {
 scene= new THREE.Scene();
 scene.background = new THREE.Color(0xdddddd);
 camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
+camera.rotation.y =  45/180*Math.PI;
+camera.position.x = 2;
+camera.position.y = 2;
+camera.position.z = 2;
 
 hlight = new THREE.AmbientLight(0x404040, 100);
 scene.add(hlight);
