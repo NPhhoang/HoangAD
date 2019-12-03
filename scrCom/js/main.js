d9f2865dcc564b8c5c03712b588966d5def7d5b3
NPhHoang <hoangdqnp@gmail.com>
2019-12-03 22:12:01 +0700

    master

--- scrCom/js/main.js
+++ scrCom/js/main.js
@@ -1,4 +1,5 @@
 //import { ColladaLoader } from 'scrCom/js/ColladaLoader.js';
+
 var scene = new THREE.Scene();
 			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
